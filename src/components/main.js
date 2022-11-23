6308b6870c762a5e45a32654181fe8c356d00b30
devWeibel <weibel.jason@gmail.com>
2022-11-23 10:58:25 -0500

    Main.js skeleton

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Projects from './Projects';
+import Resume from './Resume';
+
+export default function Main() {
+    return (
+        <Router>
+            <div>
+                
+            </div>
+        </Router>
+    );
+};
