4b9bceaf256b556df69871d601df622eb7788895
devWeibel <weibel.jason@gmail.com>
2022-11-23 10:42:30 -0500

    App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import CSS here
+import Website from './components/Website';
+
+function App() {
+    return (
+        <Website />
+    );
+};
+
+export default App;
