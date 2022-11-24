7be26283ed4df14dbfff80a40e1f2942622024f9
devWeibel <weibel.jason@gmail.com>
2022-11-23 22:31:44 -0500

    about blurb

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About () {
     return (
         <div>
             <h1>
-                time to make this look decent
+            The past ten years, I have been working as a commercial real estate appraiser. Before that, I attended Vermont Law School where I studied environmental law and received a JD and Master of Environmental Law and Policy. My undergraduate education took place at the University of Georgia where I majored in English with a minor in History and a specialization in Medieval Studies. My strengths in all of these subjects are centered in skills surrounding reading, writing, editing, language, and research. In 2022, I completed the Full Stack Coding Bootcamp at the Georgia Institute of Technology where I learned MERN stack development. While not applying directly, my previous experience demonstrates expertise at research, parsing dense documentation, and attention to detail - all beneficial skills for development.
             </h1>
         </div>
     );
